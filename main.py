2309c83ff98be52c8ae024583a4ef10394b11ab7
Suryansh Chandel <suryanshchandel07@gmail.com>
2022-11-30 23:49:24 +0530

    Python project -12

---
+++ main.py
@@ -0,0 +1,21 @@
+#def add():
+    #num1 = int(input("Enter the First Num"))
+    #num2 = int(input("Enter the Second Num"))
+    #print("Sum of two numbers is: ", num1+num2)
+
+#add() #funtion Coll
+#add()
+
+#0R
+#Funtion defination
+
+def add(num1,num2):
+    print("Sum of two numbers is: ", num1+num2)
+
+
+num1 = int(input("Enter the first Num:"))
+num2 = int(input("Enter the Second Num:"))
+add(num1,num2)
+
+
+
